91b2af188e5133363a99cd5f551aae204153d41e
hemtory <jyslshsgdy@gmail.com>
2020-07-07 17:17:33 +0900

    branch 후 merge 테스트

--- game2/setTable.js
+++ game2/setTable.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const table = document.querySelector('.table');
 
 let isGenRandomNum = true;
@@ -51,58 +50,4 @@ function genRandomNum(){
     }
 }
 
-=======
-const table = document.querySelector('.table');
-
-let isGenRandomNum = true;
-
-function setTable(){
-    let temp = 0;
-
-    for(let i = 0; i < 25; i++){
-        if(i % 5 === 0){
-            temp += 1;
-
-            const trTemp = document.createElement('tr');
-            trTemp.id = `tr${temp}`;
-            table.appendChild(trTemp);
-        }
-
-        const tr = document.querySelector(`#tr${temp}`);
-        const tdTemp = document.createElement('td');
-        tdTemp.id = `td${i}`;
-        tr.appendChild(tdTemp);
-    }
-}
-
-function genRandomNum(){
-    const numArr = [];
-    const num = 25
-
-    document.querySelector('.table').classList.add('setNum');
-
-    if(isGenRandomNum){
-        isGenRandomNum = false;
-
-        for(let i = 1; i < num + 1; i++){
-            numArr.push(i);
-        }
-    
-        for(var i = 0; i < num ; i++)
-        {
-            let temp = 0;
-            const ranNum = Math.floor(Math.random() * 25);
-            temp = numArr[i];
-            numArr[i] = numArr[ranNum];
-            numArr[ranNum] = temp;
-        }
-    
-        for(let i = 0; i < num; i++){
-            document.querySelector(`#td${i}`).innerText = `${numArr[i]}`;
-            document.querySelector(`#td${i}`).classList.add('isPlaying');
-        }
-    }
-}
-
->>>>>>> main_page
 setTable();
